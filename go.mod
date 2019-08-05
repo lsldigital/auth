d0e8e48776f38d2f2fc81a8df1b1137984becbf2
Jules Mikeal <julesmichaelgiovanni@gmail.com>
2019-08-05 17:14:56 +0400

    auth passport implementation

--- go.mod
+++ go.mod
@@ -1,17 +1,12 @@
 module go.lsl.digital/lardwaz/auth
 
+go 1.12
+
 require (
-	github.com/DataDog/zstd v1.4.0 // indirect
-	github.com/Sereal/Sereal v0.0.0-20190618215532-0b8ac451a863 // indirect
-	github.com/asdine/storm v2.1.2+incompatible
-	github.com/avct/uasurfer v0.0.0-20190117143508-20108c3c336b
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/golang/protobuf v1.3.2 // indirect
-	github.com/golang/snappy v0.0.1 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
-	github.com/stretchr/testify v1.3.0 // indirect
-	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
-	go.etcd.io/bbolt v1.3.2 // indirect
-	google.golang.org/appengine v1.6.1 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	github.com/fatih/structs v1.1.0
+	github.com/sirupsen/logrus v1.4.2
+	go.lsl.digital/lardwaz/sdk v0.0.0-00010101000000-000000000000
+	go.lsl.digital/passport/sdk v0.0.5
 )
+
+replace go.lsl.digital/lardwaz/sdk => ../lardwaz-sdk
---
+++ providers/passport/passport.go
@@ -0,0 +1,96 @@
+package passport
+
+import (
+	"context"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/fatih/structs"
+	authsdk "go.lsl.digital/lardwaz/sdk/auth"
+	passportsdk "go.lsl.digital/passport/sdk/auth"
+)
+
+// Provider implements the Provider interface
+type Provider struct {
+	name       string
+	logger     *log.Logger
+	cookieName string
+	endpoints  *authsdk.Endpoints
+	Client     *passportsdk.Client
+}
+
+// New returns a new passport provider
+func New(creds authsdk.Credentials, cookieName string, logger *log.Logger) *Provider {
+	// Init logger
+	if logger == nil {
+		logger = log.New()
+	}
+
+	// Init insecure client
+	client, err := passportsdk.NewInsecureClient(creds)
+	if err != nil {
+		logger.Errorf("Error creating new auth client: %v", err)
+		return nil
+	}
+
+	return &Provider{
+		name:       "passport",
+		logger:     logger,
+		cookieName: cookieName,
+		Client:     client,
+	}
+}
+
+// Name returns the name of provider
+func (p Provider) Name() string {
+	return p.name
+}
+
+// Endpoints returns the provider endpoints
+func (p Provider) Endpoints() *authsdk.Endpoints {
+	return p.endpoints
+}
+
+// SetEndpoints sets the provider endpoints
+func (p *Provider) SetEndpoints(endpoints *authsdk.Endpoints) {
+	p.endpoints = endpoints
+}
+
+// Session returns a Session
+func (p Provider) Session(req *http.Request) (authsdk.Session, error) {
+	cookie, err := req.Cookie(p.cookieName)
+	if err != nil {
+		p.logger.Errorf("%v: %v", authsdk.ErrTokenNotFound, err)
+		return nil, authsdk.ErrTokenNotFound
+	}
+
+	token := cookie.Value
+
+	gReq := &passportsdk.GetInfoRequest{
+		Key: token,
+	}
+
+	gResp, err := p.Client.Wire.GetInfo(context.Background(), gReq)
+	if err != nil {
+		p.logger.Errorf("%v: %v", authsdk.ErrFetchUserData, err)
+		return nil, authsdk.ErrFetchUserData
+	}
+
+	raw := structs.Map(gResp)
+
+	user := authsdk.User{
+		RawData:   raw,
+		Token:     token,
+		Provider:  p.name,
+		UserID:    gResp.GetUserID(),
+		Username:  gResp.GetUsercode(),
+		Email:     gResp.GetEmail(),
+		FirstName: gResp.GetFirstname(),
+		LastName:  gResp.GetLastname(),
+		Types:     gResp.GetRoles(),
+		Actions:   gResp.GetPermissions(),
+	}
+
+	return NewSession(user), nil
+}
